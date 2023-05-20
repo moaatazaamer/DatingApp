1d73698cd5d67035a64be50f8b87477a0b92489c
moaatazaamer <moaataz.aamer@gmail.com>
2023-05-20 20:53:17 +0400

    Commit Changes With Angualr

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,8 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +12,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 //Manually added (for dependency injection)
+builder.Services.AddCors();
 builder.Services.AddDbContext<DataContext>(opt => 
 {
 opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -20,11 +23,16 @@ opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseCors(builder => builder.WithOrigins("https://localhost:4200")
+                                  .AllowAnyHeader()
+                                  .AllowAnyMethod());
+
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
 
